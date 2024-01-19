2c31e05c289e1aaf8725b96b0d26ef8eebd59f82
gustavor101 <gustavor101@gmail.com>
2024-01-18 21:37:03 -0600

    Update butane_FUNN.py
    Add output and pickle file

--- hoomd3/classic/butane/funn/butane_FUNN.py
+++ hoomd3/classic/butane/funn/butane_FUNN.py
@@ -14,8 +14,10 @@ from math import pi, sqrt
 import gsd
 import gsd.hoomd
 import hoomd
+import matplotlib.pyplot as plt
 import hoomd.md
 import numpy as np
+import dill as pickle
 
 import pysages
 from pysages import Grid
@@ -292,12 +294,27 @@ def main(argv=[]):
     timesteps = args.time_steps
 
     tic = time.perf_counter()
-    pysages.run(method, generate_context, timesteps)
+    raw_result = pysages.run(method, generate_context, timesteps)
     toc = time.perf_counter()
     print(f"Completed the simulation in {toc - tic:0.4f} seconds.")
 
-    return method
+    result = pysages.analyze(raw_result)
 
+    mesh = result["mesh"]
+    A = result["free_energy"]
+
+    fig, ax = plt.subplots()
+
+    ax.set_xlabel(r"Dihedral Angle, $\xi$")
+    ax.set_ylabel(r"$A(\xi)$")
+
+    ax.plot(mesh, A)
+    plt.gca()
+    fig.savefig("butane-fe.png")
+
+    # write simulation to pickle file
+    pickle.dump( raw_result, open("raw_result.pickle", "wb") )
+    return result["free_energy"]
 
 # %%
 if __name__ == "__main__":
