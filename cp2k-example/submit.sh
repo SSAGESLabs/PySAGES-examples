b6575b0925fb8b0e592fb972df61870285e705e9
gustavor101 <gustavor101@gmail.com>
2024-02-23 17:53:45 -0600

    Add Ase example in paper
    Add example

---
+++ cp2k-example/submit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+#SBATCH -J WI-10
+#SBATCH -A m1266
+#SBATCH -N 1
+#SBATCH -c 4
+#SBATCH -n 64
+#SBATCH -C cpu
+#SBATCH -q debug
+##SBATCH -p <your partition>
+#SBATCH --time 10
+#SBATCH -o Report-%j.out
+
+export CP2K_DATA_DIR=$CONDA_PREFIX/cp2k-data
+export OMP_NUM_THREADS=2
+export ASE_CP2K_COMMAND="mpirun -np 64 $CONDA_PREFIX/bin/cp2k_shell.psmp"
+
+ulimit -s unlimited
+
+python sim.py
